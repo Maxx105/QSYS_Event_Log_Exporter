ded521b5de00b4d63bc334982d0d36dd07a4dc2f
Maxx Sanner <maxxsanner105@gmail.com>
2022-02-01 09:31:51 -0500

    changed info to make me author and generator new guid

--- info.lua
+++ info.lua
@@ -2,7 +2,7 @@ PluginInfo = {
   Name = "My Plugins~Log Exporter",
   Version = "1.0",
   BuildVersion = "1.1.0.0",
-  Id = "1e886705-4f8b-403e-9a2f-02bdb899a0db",
+  Id = "05011a7b-a629-42a6-a561-88f141787f06",
   Author = "Maxx Sanner",
   Description = "A simple plugin to export the Core's Event Log to a .csv"  
 }
